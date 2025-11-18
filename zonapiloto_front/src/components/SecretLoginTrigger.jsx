93d9001e553a2ebed3e26a838a2667f17884396d
Santiago Mendoza <santiagoyasno@gmail.com>
2025-11-18 17:35:23 -0500

    update

--- zonapiloto_front/src/components/SecretLoginTrigger.jsx
+++ zonapiloto_front/src/components/SecretLoginTrigger.jsx
@@ -1,29 +1,37 @@
-
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function SecretLoginTrigger() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        let sequence = [];
-        const target = ["ArrowUp", "ArrowRight", "ArrowDown", "ArrowLeft"];
+  useEffect(() => {
+    let sequence = [];
+    const target = [
+      "arrowup",
+      "arrowup",
+      "arrowdown",
+      "arrowdown",
+      "arrowleft",
+      "arrowright",
+      "arrowleft",
+      "arrowright",
+      "b",
+      "a",
+    ];
 
-        const handleKey = (e) => {
-            sequence.push(e.key);
+    const handleKey = (e) => {
+      sequence.push(e.key.toLowerCase());
 
+      if (sequence.length > target.length) sequence.shift();
 
-            if (sequence.length > 4) sequence.shift();
+      if (JSON.stringify(sequence) === JSON.stringify(target)) {
+        navigate("/loggin");
+      }
+    };
 
+    window.addEventListener("keydown", handleKey);
+    return () => window.removeEventListener("keydown", handleKey);
+  }, [navigate]);
 
-            if (JSON.stringify(sequence) === JSON.stringify(target)) {
-                navigate("/loggin");
-            }
-        };
-
-        window.addEventListener("keydown", handleKey);
-        return () => window.removeEventListener("keydown", handleKey);
-    }, [navigate]);
-
-    return null;
+  return null;
 }
